5ba048cd0350e2392df1d7205729a2a09baaa270
valselee <sollleky72@gmail.com>
2023-07-10 14:28:48 +0900

    [Init] Project init

---
+++ DreamEgg/DreamEgg/DreamEggApp.swift
@@ -0,0 +1,18 @@
+//
+//  DreamEggApp.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct DreamEggApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
