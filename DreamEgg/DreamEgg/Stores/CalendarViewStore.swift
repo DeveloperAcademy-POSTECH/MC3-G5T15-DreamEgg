d0b637eef93bc4f8be35efcf052fbcd4cc646328
valselee <sollleky72@gmail.com>
2023-07-10 16:54:50 +0900

    [Added] 1. Extensions: Date+Calculate 파일 생성 2. CalendarViewStore 파일 생성 3. DECalendarView 파일 생성

---
+++ DreamEgg/DreamEgg/Stores/CalendarViewStore.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarViewStore.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/10.
+//
+
+import Foundation
---
+++ DreamEgg/DreamEgg/Views/Components/DECalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  DECalendarView.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/10.
+//
+
+import SwiftUI
+
+struct DECalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DECalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        DECalendarView()
+    }
+}
