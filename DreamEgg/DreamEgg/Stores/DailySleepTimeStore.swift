400117290f6607af9c2d7c87f0e1549869d55ae0
valselee <sollleky72@gmail.com>
2023-07-11 17:32:26 +0900

    [Add] DailySleepTimeStore, Test View 파일 생성

---
+++ DreamEgg/DreamEgg/Stores/DailySleepTimeStore.swift
@@ -0,0 +1,8 @@
+//
+//  DailySleepTimeStore.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/11.
+//
+
+import Foundation
---
+++ DreamEgg/DreamEgg/Views/TestViews/CoreDataTestView.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataTestView.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/10.
+//
+
+import SwiftUI
+
+struct CoreDataTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoreDataTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoreDataTestView()
+    }
+}
