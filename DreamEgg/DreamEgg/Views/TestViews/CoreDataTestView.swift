f61c7834ddf2f93843506d7f2eb32cc62357d2a7
valselee <sollleky72@gmail.com>
2023-07-11 20:24:28 +0900

    [Feat/#12] 1. CoreData 기능이 제대로 작동하는지 확인하고 테스트할 수 있는 View 생 2. NavigationLink와 .navigationDestination 수식어로 dailySleepInfo를 각 하위 뷰로 전달하고 뷰를 그릴 때 활용하도록 구현

--- DreamEgg/DreamEgg/Views/TestViews/CoreDataTestView.swift
+++ DreamEgg/DreamEgg/Views/TestViews/CoreDataTestView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct CoreDataTestView: View {
+    @ObservedObject private var dailySleepTimeStore = DailySleepTimeStore(
+        coreDataStore: .debugShared
+    )
+    
+    @State private var animalName: String = ""
+    @State private var sleepTime: String = ""
+    @State private var date: Date = .now
+    @State private var eachAnimal: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("CoreData+DailySleepTime")
+            }
+            
+            ScrollView {
+                TextField("동물", text: $animalName)
+                DatePicker("", selection: $date)
+                TextField("수면시간", text: $sleepTime)
+                
+                Button {
+                    dailySleepTimeStore.updateAndSave(
+                        with: DailySleepInfo(
+                            id: .init(),
+                            animalName: animalName,
+                            date: date,
+                            sleepTime: sleepTime
+                        )
+                    )
+                } label: {
+                    Text("Sleep Data 저장")
+                }
+                
+                ForEach(dailySleepTimeStore.dailySleepArr) { dailySleepInfo in
+                    NavigationLink(value: dailySleepInfo) {
+                        Text("\(dailySleepInfo.date?.formatted() ?? "") 날짜 보러가기")
+                    }
+                }
+                .navigationDestination(for: DailySleep.self) { dailySleepInfo in
+                    Text("그 날짜의 동물은 \(dailySleepInfo.animalName ?? "")")
+                }
+            }
+        }
     }
 }
 
 struct CoreDataTestView_Previews: PreviewProvider {
     static var previews: some View {
-        CoreDataTestView()
+        NavigationStack {
+            CoreDataTestView()
+        }
     }
 }
