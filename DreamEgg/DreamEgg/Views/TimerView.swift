92fe45964649d02be1b860c8a8d03b02de0d466f
Gxxunx <g950613@naver.com>
2023-07-13 14:01:58 +0900

    Feat/#13 feat timer

---
+++ DreamEgg/DreamEgg/Views/TimerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerView.swift
+//  DreamEgg
+//
+//  Created by apple_grace_goeun on 2023/07/11.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    
+    var body: some View {
+
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
