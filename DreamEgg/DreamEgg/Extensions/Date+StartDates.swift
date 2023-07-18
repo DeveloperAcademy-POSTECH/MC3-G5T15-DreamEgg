97dade8848176985567669095f79b807fd065204
valselee <sollleky72@gmail.com>
2023-07-18 23:21:57 +0900

    [Feat/#31] 1. Month의 시작, 끝나는 날짜를 받는 메소드 구현 2. 특정 캘린더를 기준으로 2개의 날짜가 같은 달에 속하는지 확인하는 메소드 구현

--- DreamEgg/DreamEgg/Extensions/Date+StartDates.swift
+++ DreamEgg/DreamEgg/Extensions/Date+StartDates.swift
@@ -9,9 +9,9 @@ import Foundation
 
 extension Date {
     /**
-     Date instance가 포함된 이번 달, 이번 년도를 리턴합니다.
+     인스턴스의 월에서 첫번째 날을 리턴합니다.
      */
-    func startOfMonth(using calendar: Calendar) -> Date {
+    func startDayOfMonth(using calendar: Calendar) -> Date {
         calendar.date(
             from: calendar.dateComponents(
                 [.year, .month],
@@ -19,7 +19,14 @@ extension Date {
             )
         ) ?? self
     }
-        
+    
+    func lastDayOfMonth(using calendar: Calendar) -> Date {
+            return calendar.date(
+                byAdding: DateComponents(month: 1, day: -1),
+                to: self.startDayOfMonth(using: calendar)
+            )!
+        }
+    
     /**
      Date instance가 갖는 시간과 분을 리턴합니다.
      */
--- DreamEgg/DreamEgg/Extensions/Date+Utilities.swift
+++ DreamEgg/DreamEgg/Extensions/Date+Utilities.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Date {
+    public static var allComponents: Set<Calendar.Component> = [.era, .year, .month, .day, .hour, .minute, .second, .weekday, .weekdayOrdinal, .quarter, .weekOfMonth, .weekOfYear, .yearForWeekOfYear, .nanosecond, .calendar, .timeZone]
+
+    public func allComponents(in calendar: Calendar) -> DateComponents {
+        return calendar.dateComponents(Date.allComponents, from: self)
+    }
+
+    public static func isSameMonth(in calendar: Calendar, _ date1: Date, _ date2: Date) -> Bool {
+        return (date1.allComponents(in: calendar).year == date2.allComponents(in: calendar).year) &&
+        (date1.allComponents(in: calendar).month == date2.allComponents(in: calendar).month)
+    }
+}
