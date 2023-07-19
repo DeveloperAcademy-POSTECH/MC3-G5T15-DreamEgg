85ab16dedb97fcdfb60aaed490de0d258bc29a19
valselee <sollleky72@gmail.com>
2023-07-20 01:24:21 +0900

    [Add/#36] 1. Lofi Layout을 위한 File과 SVG Asset 추가

---
+++ DreamEgg/DreamEgg/Views/Lofi/LofiAwakeView.swift
@@ -0,0 +1,20 @@
+//
+//  LofiDarkeningView.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/19.
+//
+
+import SwiftUI
+
+struct LofiDarkeningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LofiDarkeningView_Previews: PreviewProvider {
+    static var previews: some View {
+        LofiDarkeningView()
+    }
+}
---
+++ DreamEgg/DreamEgg/Views/Lofi/LofiSleepGuideView.swift
@@ -0,0 +1,20 @@
+//
+//  LofiSleepGuideView.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/19.
+//
+
+import SwiftUI
+
+struct LofiSleepGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LofiSleepGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        LofiSleepGuideView()
+    }
+}
