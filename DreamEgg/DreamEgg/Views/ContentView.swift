5365ab17cae839baaee6c56e5df759fcda19b1e6
valselee <sollleky72@gmail.com>
2023-07-20 01:29:53 +0900

    [Feat/#36] 1. 앱 진입점 설정과 열거형으로 유저의 사용 상태 변경 구현 2. 처음 쓴 유저와 여러 번 쓴 유저의 Navigation 계층 분리 3. Raw Splash 구현

--- DreamEgg/DreamEgg/Views/ContentView.swift
+++ DreamEgg/DreamEgg/Views/ContentView.swift
@@ -6,11 +6,65 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
+    enum DEViewCycle: String {
+        case splash
+        case starter
+        case general
+    }
+    
+    @State private var viewCycle: DEViewCycle = .splash
+    
     var body: some View {
-        Text("Hello World")
+        switch viewCycle {
+        case .splash:
+            splashMock
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        withAnimation {
+                            // MARK: 추후 조건분기 필요
+                            viewCycle = .starter
+                        }
+                    }
+                }
+            
+        case .starter:
+            SleepTimeSettingView()
+                .frame(maxWidth: .infinity)
+            
+        case .general:
+            NavigationStack {
+                MainEggsView(countDown: 5)
+            }
+        }
+    }
+    
+    private var splashMock: some View {
+        ZStack {
+            GradientBackgroundView()
+            VStack {
+                Spacer()
+                
+                Image("DreamEggIcon")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("Dream Egg")
+                    .font(.dosPilgi(.title))
+                    .foregroundColor(.primary)
+                    .colorInvert()
+                
+                Spacer()
+                
+                Text("Copyright 2023 K-Jammy All rights reserved.")
+                    .font(.dosIyagiBold(.caption))
+                    .foregroundColor(.primary)
+                    .colorInvert()
+            }
+            .padding()
+        }
     }
 }
 
