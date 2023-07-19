a32808fe54b1cff24c997d76db2a818635756d0e
eratchacha <papa021326@naver.com>
2023-07-19 15:33:58 +0900

    [Feat/#25] Add TimePickerTestView

---
+++ DreamEgg/DreamEgg/Views/TestViews/TimePickerTestView.swift
@@ -0,0 +1,20 @@
+//
+//  TimePickerTestView.swift
+//  DreamEgg
+//
+//  Created by 차차 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct TimePickerTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimePickerTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimePickerTestView()
+    }
+}
