48c98893f183a16b9d6959574659c91383554abd
valselee <sollleky72@gmail.com>
2023-07-19 15:01:10 +0900

    [Feat/#31] 1. DECalendar TestView 구현 2. Font에 Kerning과 spacing을 절댓값으로 부여하여 디자인 작업 3. 가로 스크롤, 주, 월 이동은 추후에 구현

--- DreamEgg/DreamEgg/Views/TestViews/DECalendarTestView.swift
+++ DreamEgg/DreamEgg/Views/TestViews/DECalendarTestView.swift
@@ -8,63 +8,75 @@
 import SwiftUI
 
 struct DECalendarTestView: View {
-    private let calendar: Calendar
+    private var calendar: Calendar
     private let monthDayFormatter: DateFormatter
     private let dayFormatter: DateFormatter
     private let weekDayFormatter: DateFormatter
     private let timeFormatter: DateFormatter
     
-    @State private var selectedDate = Self.now
-    private static var now = Date() // Cache now
+    @State private var selectedDate: Date = Date.now
     
     var body: some View {
-        DECalendar(
-            style: .week,
-            calendar: calendar,
-            date: $selectedDate
-        ) { date in
-            Button {
-                selectedDate = date
-            } label: {
-                VStack(spacing: 15) {
-                    Text(weekDayFormatter.string(from: date))
-                        .font(.dosIyagiBold(.body))
-                    
-                    Text(dayFormatter.string(from: date))
-                        .font(.dosIyagiBold(.body))
-                        .fontWeight(.semibold)
-                }
-                .foregroundStyle(calendar.isDate(date, inSameDayAs: selectedDate) ? .primary : .secondary)
-                .foregroundColor(calendar.isDate(date, inSameDayAs: selectedDate) ? .white : .black)
-                .frame(maxWidth: 45, maxHeight: 80)
-                .background {
-                    if calendar.isDate(
-                        date,
-                        inSameDayAs: selectedDate
-                    ) {
-                        Capsule()
-                            .fill(Color.dreamPurple)
+        VStack {
+            DECalendar(
+                style: .week,
+                calendar: calendar,
+                date: $selectedDate
+            ) { date in
+                Button {
+                    selectedDate = date
+                } label: {
+                    VStack(spacing: 12) {
+                        Text(weekDayFormatter.string(from: date))
+                        
+                        Text(dayFormatter.string(from: date))
+                    }
+                    .font(.dosIyagiBold(.body))
+                    .foregroundStyle(calendar.isDate(date, inSameDayAs: selectedDate) ? .primary : .secondary)
+                    .foregroundColor(calendar.isDate(date, inSameDayAs: selectedDate) ? .white : .black)
+                    .frame(maxWidth: 45, maxHeight: 75)
+                    .background {
+                        if calendar.isDate(
+                            date,
+                            inSameDayAs: selectedDate
+                        ) {
+                            Capsule()
+                                .fill(Color.dreamPurple)
+                        }
                     }
                 }
-            }
-        } monthHeader: {
-            VStack(alignment: .leading, spacing: 5) {
-                Text("\(monthDayFormatter.string(from: selectedDate))월")
-                    .font(.dosIyagiBold(.title))
-                    
-                Text("\(selectedDate.weekOfMonth(using: calendar))주차")
-                    .font(.dosIyagiBold(.callout))
+            } monthHeader: { eachWeeksDays in
+                let firstDayInWeek = eachNthWeekFirstDay(eachWeeksDays)
+                let lastDayInWeek = eachNthWeekLastDay(eachWeeksDays)
                 
-            }
-            .frame(
-                maxWidth: .infinity,
-                alignment: .leading
-            )
-            .padding()
-            
-        } weekSwitcher: {
-            Button {
-                withAnimation(.easeIn) {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("\(monthDayFormatter.string(from: selectedDate))월")
+                        .font(.dosIyagiBold(.title))
+                    
+                    HStack(spacing: -4) {
+                        Text("\(selectedDate.weekOfMonth(using: calendar))주차")
+                        
+                        Text(" (\(monthDayFormatter.string(from: selectedDate)).\(dayFormatter.string(from: firstDayInWeek)).")
+                            .kerning(-0.75)
+                        
+                        Text(" ~ ")
+                            .baselineOffset(-6)
+                            
+                        
+                        Text("\(monthDayFormatter.string(from: selectedDate)).\(dayFormatter.string(from: lastDayInWeek)).)")
+                            .kerning(-0.75)
+                    }
+                    .font(.dosIyagiBold(.callout))
+                   
+                }
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                .padding()
+                
+            } weekSwitcher: {
+                Button {
                     guard let newDate = calendar.date(
                         byAdding: .weekOfMonth,
                         value: -1,
@@ -74,13 +86,12 @@ struct DECalendarTestView: View {
                     }
                     
                     selectedDate = newDate
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .padding(.horizontal)
+                        .tint(.subButtonBlue)
                 }
-            } label: {
-                Image(systemName: "chevron.left")
-                    .padding(.horizontal)
-            }
-            Button {
-                withAnimation(.easeIn) {
+                Button {
                     guard let newDate = calendar.date(
                         byAdding: .weekOfMonth,
                         value: 1,
@@ -90,21 +101,42 @@ struct DECalendarTestView: View {
                     }
                     
                     selectedDate = newDate
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .padding(.horizontal)
+                        .tint(.subButtonBlue)
                 }
-            } label: {
-                Image(systemName: "chevron.right")
-                    .padding(.horizontal)
             }
         }
+        
     }
     
     // MARK: LifeCycle
     init(calendar: Calendar = .init(identifier: .gregorian)) {
-        self.calendar = calendar
-        self.monthDayFormatter = DateFormatter(dateFormat: "MM", calendar: calendar)
+        var temp = calendar
+        temp.timeZone = .gmt
+        self.calendar = temp
+        self.monthDayFormatter = DateFormatter(dateFormat: "M", calendar: calendar)
         self.dayFormatter = DateFormatter(dateFormat: "d", calendar: calendar)
         self.weekDayFormatter = DateFormatter(dateFormat: "EEE", calendar: calendar)
         self.timeFormatter = DateFormatter(dateFormat: "H:mm", calendar: calendar)
+    }
+    
+    // MARK: Methods
+    private func eachNthWeekFirstDay(_ dates: [Date]) -> Date {
+        if Date.isSameMonth(in: calendar, selectedDate, dates.first!) {
+            return dates.first!
+        } else {
+            return selectedDate.startDayOfMonth(using: calendar)
+        }
+    }
+    
+    private func eachNthWeekLastDay(_ dates: [Date]) -> Date {
+        if Date.isSameMonth(in: calendar, selectedDate, dates.last!) {
+            return dates.last!
+        } else {
+            return selectedDate.lastDayOfMonth(using: calendar)
+        }
     }
 }
 
