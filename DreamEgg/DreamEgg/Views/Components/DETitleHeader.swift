1100fb32f52756d1f6437ac5ddb5b9666c312616
valselee <sollleky72@gmail.com>
2023-07-22 15:49:36 +0900

    [Add/#36] DETitleHeader 파일 생성

---
+++ DreamEgg/DreamEgg/Views/Components/DETitleHeader.swift
@@ -0,0 +1,20 @@
+//
+//  DETitleHeader.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/21.
+//
+
+import SwiftUI
+
+struct DETitleHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DETitleHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        DETitleHeader()
+    }
+}
