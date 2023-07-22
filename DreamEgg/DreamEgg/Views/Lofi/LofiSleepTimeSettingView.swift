943fc44fa05614827e91885aaf2f64f118017957
valselee <sollleky72@gmail.com>
2023-07-23 01:20:18 +0900

    [Add/#45] Lofi UT를 위한 Sleep Time Setting View 생성

---
+++ DreamEgg/DreamEgg/Views/Lofi/LofiSleepTimeSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  LofiSleepTimeSettingView.swift
+//  DreamEgg
+//
+//  Created by Celan on 2023/07/22.
+//
+
+import SwiftUI
+
+struct LofiSleepTimeSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LofiSleepTimeSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LofiSleepTimeSettingView()
+    }
+}
