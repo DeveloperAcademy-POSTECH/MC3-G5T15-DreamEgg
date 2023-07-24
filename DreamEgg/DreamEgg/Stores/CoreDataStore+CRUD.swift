73b14fb5f1ad68c08ee867e9383a12223c7f6915
valselee <sollleky72@gmail.com>
2023-07-24 14:34:52 +0900

    [Feat/#45] 1. CoreData CRUD를 추상화하여 protocol 확장 구현 2. 호출 테스트 진행 필요

--- DreamEgg/DreamEgg/Stores/CoreDataStore+CRUD.swift
+++ DreamEgg/DreamEgg/Stores/CoreDataStore+CRUD.swift
@@ -7,34 +7,7 @@
 
 import CoreData
 
-extension CoreDataStore {
-    private func fetchFirst<T: NSManagedObject>(
-        _ objectType: T.Type,
-        predicate: NSPredicate?
-    ) -> Result<T?, Error> {
-        let request = objectType.fetchRequest()
-        request.predicate = predicate
-        request.fetchLimit = 1
-        do {
-            let result = try managedObjectContext.fetch(request) as? [T]
-            return .success(result?.first)
-        } catch {
-            return .failure(error)
-        }
-    }
-}
-
-extension CoreDataStore {
-    private func saveData() {
-        if managedObjectContext.hasChanges {
-            do {
-                try managedObjectContext.save()
-            } catch let error as NSError {
-                NSLog("Unresolved error saving context: \(error), \(error.userInfo)")
-            }
-        }
-    }
-    
+extension CoreDataStore: CoreDataRepresentable {
     public func updateUserSleepConfig(
         with userSleepConfig: UserSleepConfigurationInfo
     ) {
@@ -43,15 +16,16 @@ extension CoreDataStore {
             userSleepConfig.id as CVarArg
         )
         
-        let result = fetchFirst(
+        let result = fetchCoreData(
             UserSleepConfiguration.self,
-            predicate: predicate
+            predicate: predicate,
+            fetchLimit: 1
         )
         
         switch result {
         case let .success(managedUserSleepConfig):
             // 기존 저장된 CoreData Entity가 있다면 속성 할당하고 곧바로 업데이트
-            if let managedUserSleepConfig {
+            if let managedUserSleepConfig = managedUserSleepConfig?.first {
                 assignConfigProperties(
                     managedUserSleepConfig: managedUserSleepConfig,
                     from: userSleepConfig
@@ -64,7 +38,7 @@ extension CoreDataStore {
             print("Couldn't fetch TodoMO to save")
         }
         
-        saveData()
+        saveChangesToContext()
     }
     
     private func saveUserSleepConfig(from config: UserSleepConfigurationInfo) {
@@ -72,7 +46,6 @@ extension CoreDataStore {
             context: managedObjectContext
         )
         
-        managedUserSleepConfig.id = config.id
         assignConfigProperties(
             managedUserSleepConfig: managedUserSleepConfig,
             from: config
@@ -83,6 +56,7 @@ extension CoreDataStore {
         managedUserSleepConfig: UserSleepConfiguration,
         from configInfo: UserSleepConfigurationInfo
     ) {
+        managedUserSleepConfig.id = configInfo.id
         managedUserSleepConfig.targetSleepTime = configInfo.targetSleepTime
         managedUserSleepConfig.notificationMessage = configInfo.notificationMessage
     }
