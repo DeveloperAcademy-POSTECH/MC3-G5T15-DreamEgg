10956c3d3e8befecece380de6431ce80e7d30eb2
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-07-24 18:13:30 +0900

    [Feat/#56] ZStack에서 overlay로 구조 변경

--- DreamEgg/DreamEgg/Views/Lofi/LofiAwakeView.swift
+++ DreamEgg/DreamEgg/Views/Lofi/LofiAwakeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct LofiAwakeView: View {
     @State private var currentTime: Date = .now
     @State var maskColor = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.8))
-    
+
     private let hourFormatter = DateFormatter(
         dateFormat: "H",
         calendar: .current
@@ -32,22 +32,17 @@ struct LofiAwakeView: View {
                 Text("지금 시간은 \n\(hourFormatter.string(from: currentTime))시 \(minuteFormatter.string(from: currentTime))분이에요.")
 //                Text("It's \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime)) now")
                 .font(.dosIyagiBold(.largeTitle))
-
 // 추후 수면시간 대비 분기처리를 위한 if 구조를 만들어두었습니다.
                 if currentTime == currentTime {
-                    ZStack {
-                        Image("EggPillow")
-                            .resizable()
-                            .frame(width: 160, height: 160)
-                            .position(
-                            x: UIScreen.main.bounds.width / 2.6,
-                            y: UIScreen.main.bounds.height / 3.6
-                        )
+                    Image("EggPillow")
+                        .resizable()
+                        .frame(width: 160, height: 160)
+                        .padding(.top, 170)
+                        .overlay(
                         Image("FerretEgg")
                             .resizable()
                             .frame(width: 160, height: 160)
-                    }
-                        .frame(width: 300, height: 300)
+                    )
                     // 나중에 currentTime 대신에 수면 시간 값이 들어가야 함.
                     Text("\(hourFormatter.string(from: currentTime))시간 \(minuteFormatter.string(from: currentTime))분 동안 알을 품었네요.\n잠은 충분히 주무셨나요?")
                     //                Text("You incubate the egg for \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime))")
@@ -80,35 +75,34 @@ struct LofiAwakeView: View {
                         .cornerRadius(8)
                         .padding(.horizontal)
                 } else {
-                    ZStack {
-                        
-                        Image("FerretEgg")
-                            .resizable()
-                            .frame(width: 160, height: 160)
-                            .rotationEffect(.degrees(90))
-                        
+                    Spacer()
+                    Image("FerretEgg")
+                        .resizable()
+                        .frame(width: 160, height: 160)
+                        .rotationEffect(.degrees(90))
+                        .overlay(
                         Image("EggHat")
                             .resizable()
                             .frame(width: 120, height: 120)
                             .rotationEffect(.degrees(60))
-                            .position(
-                            x: UIScreen.main.bounds.width / 1.6,
-                            y: UIScreen.main.bounds.height / 8
-                        )
+                            .padding(.bottom, 80)
+                            .padding(.leading, 180)
+                    )
+                        .overlay(
                         Image("EggPillow")
                             .resizable()
                             .frame(width: 160, height: 160)
                             .rotationEffect(.degrees(-25))
-                            .position(
-                            x: UIScreen.main.bounds.width / 3,
-                            y: UIScreen.main.bounds.height / 8
-                        )
-                    }
-                        .frame(width: 300, height: 300)
+                            .padding(.bottom, 80)
+                            .padding(.trailing, 40)
+                    )
                         .overlay(
-                            Rectangle().fill(maskColor)
-                        )
-                    
+                        Rectangle()
+                            .fill(maskColor)
+                            .frame(width: 300, height: 300)
+                    )
+
+                    Spacer()
                     // 나중에 currentTime 대신에 수면 시간 값이 들어가야 함.
                     Text("\(hourFormatter.string(from: currentTime))시간 \(minuteFormatter.string(from: currentTime))분 동안 알을 품었네요.\n좀 더 주무셔야겠어요.")
                     //                Text("You incubate the egg for \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime))")
