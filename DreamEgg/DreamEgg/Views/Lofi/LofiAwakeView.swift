455599e3410d0f02015075601c3196c4cd825b45
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-07-25 16:33:42 +0900

    [Feat/#56] 사용자가 Egg를 Tab하였을때 Confetti Animation이 나타납니다.

--- DreamEgg/DreamEgg/Views/Lofi/LofiAwakeView.swift
+++ DreamEgg/DreamEgg/Views/Lofi/LofiAwakeView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct LofiAwakeView: View {
     @State private var currentTime: Date = .now
     @State var maskColor = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.8))
+    @State private var eggTab = false
+    @State private var isButtonDisabled = false
 
     private let hourFormatter = DateFormatter(
         dateFormat: "H",
@@ -32,18 +34,42 @@ struct LofiAwakeView: View {
                 Text("지금 시간은 \n\(hourFormatter.string(from: currentTime))시 \(minuteFormatter.string(from: currentTime))분이에요.")
 //                Text("It's \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime)) now")
                 .font(.dosIyagiBold(.largeTitle))
-// 추후 수면시간 대비 분기처리를 위한 if 구조를 만들어두었습니다.
-                if currentTime == currentTime {
-                    Image("EggPillow")
-                        .resizable()
-                        .frame(width: 160, height: 160)
-                        .padding(.top, 170)
-                        .overlay(
-                        Image("FerretEgg")
-                            .resizable()
-                            .frame(width: 160, height: 160)
-                    )
-                    // 나중에 currentTime 대신에 수면 시간 값이 들어가야 함.
+
+                // 추후 수면시간 대비 분기처리를 위한 if 구조.
+                if currentTime != currentTime {
+                    ZStack {
+                        /** egg를 탭하면 eggtab의 값이 toggle되어 confetti가 적용 된 sparkle 이미지가 나타나고, 3초간 버튼 기능을 비활성합니다. 3초 후에는 각 토글을 다시 false로 전환합니다. */
+                        Button(action: {
+                            if !isButtonDisabled {
+                                eggTab = true
+                                isButtonDisabled = true
+
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                    isButtonDisabled = false
+                                    eggTab = false
+                                }
+                            }
+                        }) {
+                            Image("EggPillow")
+                                .resizable()
+                                .frame(width: 160, height: 160)
+                                .padding(.top, 170)
+                                .overlay(
+                                Image("FerretEgg")
+                                    .resizable()
+                                    .frame(width: 160, height: 160)
+                            )
+
+                        }
+                            .disabled(isButtonDisabled)
+
+                        if eggTab {
+                            Image("ShinyMiddle")
+                                .modifier(ParticlesModifier())
+                        }
+                    }
+
+                    // 추후 currentTime 대신에 수면 시작 시간으로부터 누적 된 값이 들어가야 함.
                     Text("\(hourFormatter.string(from: currentTime))시간 \(minuteFormatter.string(from: currentTime))분 동안 알을 품었네요.\n잠은 충분히 주무셨나요?")
                     //                Text("You incubate the egg for \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime))")
                     .font(.dosIyagiBold(.title3))
@@ -61,7 +87,7 @@ struct LofiAwakeView: View {
                         LofiCurtainView()
                     } label: {
                         Text("잘 잤어요!")
-                        //                    Text("I slept well!")
+//                    Text("I slept well!")
                         .frame(maxWidth: .infinity)
                             .padding(.vertical, 16)
                             .foregroundColor(.primaryButtonBrown)
@@ -102,15 +128,23 @@ struct LofiAwakeView: View {
                             .frame(width: 300, height: 300)
                     )
 
+                        .overlay(
+                        Text("Zzz...")
+                            .font(.dosIyagiBold(.callout))
+                            .padding(.top, 70)
+                            .padding(.leading, 110)
+                    )
+
                     Spacer()
-                    // 나중에 currentTime 대신에 수면 시간 값이 들어가야 함.
+
+                    // 추후 currentTime 대신에 수면 시작 시간으로부터 누적 된 값이 들어가야 함.
                     Text("\(hourFormatter.string(from: currentTime))시간 \(minuteFormatter.string(from: currentTime))분 동안 알을 품었네요.\n좀 더 주무셔야겠어요.")
-                    //                Text("You incubate the egg for \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime))")
+//                  Text("You incubate the egg for \(hourFormatter.string(from: currentTime)):\(minuteFormatter.string(from: currentTime))")
                     .font(.dosIyagiBold(.title3))
                         .padding()
 
                     Text("화면을 다시 잠그면\n알을 더 오래 품을 수 있어요.")
-                    //                Text("If you lock the screen again\nYou can incubte the egg more.")
+//                  Text("If you lock the screen again\nYou can incubte the egg more.")
                     .font(.dosIyagiBold(.body))
                         .foregroundColor(.secondary)
                         .colorInvert()
@@ -119,10 +153,10 @@ struct LofiAwakeView: View {
 
                     NavigationLink {
                         Text("실패")
-                        //                    Text("To Fail screen")
+//                      Text("To Fail screen")
                     } label: {
                         Text("아직도 잠을 못잤어요.")
-                        //                    Text("I don't feel like to sleep")
+//                      Text("I don't feel like to sleep")
                         .frame(maxWidth: .infinity)
                             .padding(.vertical, 16)
                             .foregroundColor(.subButtonBlue)
@@ -136,6 +170,49 @@ struct LofiAwakeView: View {
                 .multilineTextAlignment(.center)
                 .foregroundColor(.white)
 
+        }
+    }
+}
+
+/** confetti 애니메이션에 대한 설정 1 */
+struct FireworkParticlesGeometryEffect: GeometryEffect {
+    var time: Double
+    var speed = Double.random(in: 20 ... 200)
+    var direction = Double.random(in: -Double.pi ... Double.pi)
+
+    var animatableData: Double {
+        get { time }
+        set { time = newValue }
+    }
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let xTranslation = speed * cos(direction) * time
+        let yTranslation = speed * sin(direction) * time
+        let affineTranslation = CGAffineTransform(translationX: xTranslation, y: yTranslation)
+        return ProjectionTransform(affineTranslation)
+    }
+}
+
+/** confetti에 애니메이션에 대한 설정 2 */
+struct ParticlesModifier: ViewModifier {
+    @State var time = 0.0
+    @State var scale = 0.1
+    let duration = 5.0
+
+    func body(content: Content) -> some View {
+        ZStack {
+            ForEach(0..<80, id: \.self) { index in
+                content
+                    .hueRotation(Angle(degrees: time * 80))
+                    .scaleEffect(scale)
+                    .modifier(FireworkParticlesGeometryEffect(time: time))
+                    .opacity(((duration - time) / duration))
+            }
+        }
+            .onAppear {
+            withAnimation (.easeOut(duration: duration)) {
+                self.time = duration
+                self.scale = 1.0
+            }
         }
     }
 }
