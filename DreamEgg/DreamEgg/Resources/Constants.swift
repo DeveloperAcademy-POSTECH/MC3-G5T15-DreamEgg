e8d825381ae370568aa27ab301e8b283b06c58c5
valselee <sollleky72@gmail.com>
2023-07-27 02:13:30 +0900

    [Feat/#62] 1. CoreDataEntity 형태 변화에 따라 앱 내부의 타입에 속성 추가선언(sleepTimeInMinute, processStatus, assetName) 2. processStatus의 문자열 저장을 위한 상수형 선언 3. 속성 변경에 따라 테스트뷰 일부 수정

--- DreamEgg/DreamEgg/Resources/Constants.swift
+++ DreamEgg/DreamEgg/Resources/Constants.swift
@@ -11,4 +11,25 @@ public enum Constant {
     static let BASE_NOTIFICATION_MESSAGE = "오늘의 알을 품고 목표한 시간에 잠을 자볼까요?"
     static let BASE_TARGET_SLEEP_TIME = Date.now
     static let DREAMEGG_NOTIFICATION_ID = "DreamEggNotificationID"
+   
+    static let SLEEP_PROCESS_READY: String = "READY"
+    static let SLEEP_PROCESS_PROCESSING: String = "PROCESSING"
+    static let SLEEP_PROCESS_COMPLETE: String = "COMPLETE"
+    static let SLEEP_PROCESS_STOPPED: String = "STOPPED"
+    
+    enum DreamPets {
+        static var DREAMPET_NAME_SET: Set<String> = Set(
+            [
+                "Quokka",
+                "Rabbit",
+            ]
+        )
+        
+        static var DREAMPET_EGGNAME_SET: Set<String> = Set(
+            [
+                "BinyEgg",
+                "FerretEgg",
+            ]
+        )
+    }
 }
