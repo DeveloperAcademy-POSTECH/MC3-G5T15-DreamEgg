fb43fdab6783c819afea0c3dbbf6193ce8bc71c0
valselee <sollleky72@gmail.com>
2023-07-27 02:06:29 +0900

    [Feat/#62] 1. DECalendar가 view에 각 주차별 [Date]를 리턴하도록 수정 2. DECalendarView가 CoreData의 DailySleep을 기준으로 뷰를 그리도록 구현 완료

--- DreamEgg/DreamEgg/Views/TestViews/DECalendarTestView.swift
+++ DreamEgg/DreamEgg/Views/TestViews/DECalendarTestView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct DECalendarTestView: View {
+    @EnvironmentObject var dailySleepInfoStore: DailySleepTimeStore
+    
     private var calendar: Calendar
     private let monthDayFormatter: DateFormatter
     private let dayFormatter: DateFormatter
@@ -15,6 +17,7 @@ struct DECalendarTestView: View {
     private let timeFormatter: DateFormatter
     
     @State private var selectedDate: Date = Date.now
+    @State private var selectedWeek: [Date] = []
     
     var body: some View {
         VStack {
@@ -27,28 +30,33 @@ struct DECalendarTestView: View {
                 style: .week,
                 calendar: calendar,
                 date: $selectedDate
-            ) { date in
+            ) { eachDay, selectedWeek in
                 Button {
-                    selectedDate = date
+                    self.selectedDate = eachDay
+                    self.selectedWeek = selectedWeek
+                    dailySleepInfoStore.assignDailySleep(at: eachDay)
                 } label: {
                     VStack(spacing: 12) {
-                        Text(weekDayFormatter.string(from: date))
+                        Text(weekDayFormatter.string(from: eachDay))
                         
-                        Text(dayFormatter.string(from: date))
+                        Text(dayFormatter.string(from: eachDay))
                         
                         Image(systemName: "circle.fill")
+                            .foregroundColor(eachDayForegroundColor(eachDay))
                     }
-                    .foregroundStyle(calendar.isDate(date, inSameDayAs: selectedDate) ? .primary : .secondary)
-                    .foregroundColor(calendar.isDate(date, inSameDayAs: selectedDate) ? .white : .black)
+                    .foregroundStyle(
+                        calendar.isDate(eachDay, inSameDayAs: selectedDate) ? .primary : .secondary)
+                    .foregroundColor(calendar.isDate(eachDay, inSameDayAs: selectedDate) ? .white : .black)
                     .frame(maxWidth: 44, maxHeight: 110)
                     .background {
-                        if calendar.isDate(
-                            date,
-                            inSameDayAs: selectedDate
-                        ) {
-                            Capsule()
+                        if calendar.isDate(eachDay, inSameDayAs: selectedDate) {
+                            RoundedRectangle(cornerRadius: 8)
                                 .fill(Color.accentColor)
                         }
+                    }
+                    .onAppear {
+                        self.selectedWeek = selectedWeek
+                        dailySleepInfoStore.assignDailySleep(at: eachDay)
                     }
                 }
             } monthHeader: { eachWeeksDays in
@@ -63,7 +71,6 @@ struct DECalendarTestView: View {
                     HStack(spacing: -4) {
                         Text("\(selectedDate.weekOfMonth(using: calendar))주차")
 
-                        
                         Text(" (\(monthDayFormatter.string(from: selectedDate)).\(dayFormatter.string(from: firstDayInWeek)).")
                             .kerning(-0.75)
                         
@@ -75,7 +82,6 @@ struct DECalendarTestView: View {
                             .kerning(-0.75)
                     }
                     .font(.dosIyagiBold(.callout))
-                    
                 }
                 .frame(
                     maxWidth: .infinity,
@@ -99,6 +105,7 @@ struct DECalendarTestView: View {
                         .padding(.horizontal)
                         .tint(.subButtonBlue)
                 }
+                
                 Button {
                     guard let newDate = calendar.date(
                         byAdding: .weekOfMonth,
@@ -123,88 +130,9 @@ struct DECalendarTestView: View {
             
             ScrollView {
                 LazyVStack(pinnedViews: .sectionHeaders) {
-                    Section {
-                        HStack {
-                            Text("이 날은")
-
-                            Text("드림펫")
-                                .foregroundColor(.subButtonBlue)
-                            
-                            + Text("이 태어나지 않았어요.")
-                        }
-                        .font(.dosIyagiBold(.footnote))
-                        .frame(maxWidth: .infinity)
-                        .padding(20)
-                        .background {
-                            Capsule()
-                                .fill(Color.subButtonSky)
-                                .padding(.horizontal)
-                        }
-                    } header: {
-                        Text("Selected Day")
-                            .font(.dosIyagiBold(.title3))
-                            .foregroundColor(.white)
-                            .frame(
-                                maxWidth: .infinity,
-                                alignment: .leading
-                            )
-                            .padding(16)
-                    }
+                    selectedDaySleepInfoSection()
                     
-                    Section {
-                        HStack(alignment: .center) {
-                            Spacer()
-                                .frame(maxWidth: 20)
-                            
-                            Image("Quokka_Face")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: 80, maxHeight: 80)
-                                .background {
-                                    Circle()
-                                        .fill(Color.eggYellow)
-                                        .frame(maxWidth: 60, maxHeight: 60)
-                                }
-                                .background {
-                                    Circle()
-                                        .fill(Color.primaryButtonYellow)
-                                        .frame(maxWidth: 70, maxHeight: 70)
-                                }
-                            
-                            VStack(
-                                alignment: .leading,
-                                spacing: 8
-                            ) {
-                                Text("꿔까")
-                                    .font(.dosIyagiBold(.callout))
-                                
-                                Text("\(timeFormatter.string(from: Date.now)) 출생")
-                                    .font(.dosIyagiBold(.footnote))
-                            }
-                            
-                            Spacer()
-                                .frame(maxWidth: 30)
-                        }
-                        .frame(
-                            maxWidth: .infinity,
-                            alignment: .leading
-                        )
-                        .background {
-                            Capsule()
-                                .fill(Color.subButtonSky)
-                                .padding(.horizontal)
-                        }
-                    } header: {
-                        Text("This Week")
-                            .font(.dosIyagiBold(.title3))
-                            .foregroundColor(.white)
-                            .frame(
-                                maxWidth: .infinity,
-                                alignment: .leading
-                            )
-                            .padding(16)
-                    }
-                    
+                    selectedWeekSleepInfoSection()
                 }
             }
         }
@@ -217,6 +145,130 @@ struct DECalendarTestView: View {
         self.dayFormatter = DateFormatter(dateFormat: "d", calendar: calendar)
         self.weekDayFormatter = DateFormatter(dateFormat: "EEE", calendar: calendar)
         self.timeFormatter = DateFormatter(dateFormat: "H:mm", calendar: calendar)
+    }
+    
+    // MARK: ViewBuilders
+    private func selectedDaySleepInfoSection() -> some View {
+        Section {
+            HStack {
+                if let selectedDailySleep = dailySleepInfoStore.selectedDailySleep,
+                   hasSelectedDateWellSlept(in: selectedDailySleep) {
+                    // MARK: 3시간이 넘어야 부활한다!
+                    dailySleepCardCell(by: selectedDailySleep)
+                } else {
+                    HStack {
+                        Text("이 날은")
+
+                        Text("드림펫")
+                            .foregroundColor(.subButtonBlue)
+                        
+                        + Text("이 태어나지 않았어요.")
+                    }
+                    .font(.dosIyagiBold(.footnote))
+                    .frame(maxWidth: .infinity)
+                    .padding(20)
+                    .background {
+                        Capsule()
+                            .fill(Color.subButtonSky)
+                            .padding(.horizontal)
+                    }
+                }
+            }
+        } header: {
+            Text("선택한 날")
+                .font(.dosIyagiBold(.title3))
+                .foregroundColor(.white)
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                .padding(16)
+        }
+    }
+    
+    private func selectedWeekSleepInfoSection() -> some View {
+        Section {
+            let wellSleptArray = eachWeeksWellSleptArrayBuilder()
+            if wellSleptArray.isEmpty {
+                HStack {
+                    Text("이 주엔")
+
+                    Text("드림펫")
+                        .foregroundColor(.subButtonBlue)
+                    
+                    + Text("이 태어나지 않았어요.")
+                }
+                .font(.dosIyagiBold(.footnote))
+                .frame(maxWidth: .infinity)
+                .padding(20)
+                .background {
+                    Capsule()
+                        .fill(Color.subButtonSky)
+                        .padding(.horizontal)
+                }
+            } else {
+                ForEach(wellSleptArray) { dailySleep in
+                    dailySleepCardCell(by: dailySleep)
+                }
+            }
+            
+        } header: {
+            Text("이번 주")
+                .font(.dosIyagiBold(.title3))
+                .foregroundColor(.white)
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                .padding(16)
+        }
+    }
+    
+    private func dailySleepCardCell(
+        by dailySleep: DailySleep
+    ) -> some View {
+        HStack(alignment: .center) {
+            Spacer()
+                .frame(maxWidth: 20)
+            
+            Image("\(dailySleep.assetName!)"+"_Face")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 80, maxHeight: 80)
+                .background {
+                    Circle()
+                        .fill(Color.eggYellow)
+                        .frame(maxWidth: 60, maxHeight: 60)
+                }
+                .background {
+                    Circle()
+                        .fill(Color.primaryButtonYellow)
+                        .frame(maxWidth: 70, maxHeight: 70)
+                }
+            
+            VStack(
+                alignment: .leading,
+                spacing: 8
+            ) {
+                Text("\(dailySleep.animalName!)")
+                    .font(.dosIyagiBold(.callout))
+                
+                Text("\(dailySleep.date!.formatted()) 출생")
+                    .font(.dosIyagiBold(.footnote))
+            }
+            
+            Spacer()
+                .frame(maxWidth: 30)
+        }
+        .frame(
+            maxWidth: .infinity,
+            alignment: .leading
+        )
+        .background {
+            Capsule()
+                .fill(Color.subButtonSky)
+                .padding(.horizontal)
+        }
     }
     
     // MARK: Methods
@@ -235,6 +287,38 @@ struct DECalendarTestView: View {
             return selectedDate.lastDayOfMonth(using: calendar)
         }
     }
+    
+    private func eachDayForegroundColor(_ eachDay: Date) -> Color {
+        dailySleepInfoStore.hasWellSleptDailySleep(in: eachDay)
+            ? .eggYellow
+            : .gray
+    }
+    
+    private func eachWeeksWellSleptArrayBuilder() -> [DailySleep] {
+        dailySleepInfoStore.dailySleepArray.filter { eachDailySleep in
+            var res = false
+            for eachDay in selectedWeek {
+                res = eachDailySleep.sleepTimeInMinute >= 3 * 60 &&
+                Date.isSameDate(lhs: eachDay, rhs: eachDailySleep.date!)
+                if res { break }
+            }
+            return res
+        }
+        .sorted { lhs, rhs in
+            lhs.date! > rhs.date!
+        }
+    }
+    
+    private func hasSelectedDateWellSlept(
+        in selectedDailySleep: DailySleep
+    ) -> Bool {
+        if selectedDailySleep.sleepTimeInMinute >= 3 * 60,
+           Date.isSameDate(lhs: selectedDate, rhs: selectedDailySleep.date!) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
 struct DECalendarTestView_Previews: PreviewProvider {
@@ -244,5 +328,6 @@ struct DECalendarTestView_Previews: PreviewProvider {
             
             DECalendarTestView()
         }
+        .environmentObject(DailySleepTimeStore())
     }
 }
