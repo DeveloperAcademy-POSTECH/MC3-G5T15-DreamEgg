cde0905d17c0e1109e20de648b4becbd9da11c61
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-07-27 15:45:36 +0900

    [Feat/#75] Background 밤으로 고정

--- DreamEgg/DreamEgg/Views/Components/GradientBackgroundView.swift
+++ DreamEgg/DreamEgg/Views/Components/GradientBackgroundView.swift
@@ -11,28 +11,28 @@ import CoreData
 struct GradientBackgroundView: View {
     /** Calendar에서 가져 온 사용자의 시간 값
      */
-    let hour = Calendar.current.component(.hour, from: Date())
+//    let hour = Calendar.current.component(.hour, from: Date())
     
     // 밤에 보이는 GradientColor
     @State var startColorNightTime = Color(#colorLiteral(red: 0.6274509804, green: 0.5725490196, blue: 0.7843137255, alpha: 1))
     @State var endColorNightTime = Color(#colorLiteral(red: 0.4666666667, green: 0.5215686275, blue: 0.8156862745, alpha: 1))
 
     // 낮에 보이는 GradientColor
-    @State var startColorDayTime = Color(#colorLiteral(red: 0.6392156863, green: 0.8039215686, blue: 1, alpha: 1))
-    @State var endColorDayTime = Color(#colorLiteral(red: 1, green: 0.7960784314, blue: 0.2705882353, alpha: 1))
+//    @State var startColorDayTime = Color(#colorLiteral(red: 0.6392156863, green: 0.8039215686, blue: 1, alpha: 1))
+//    @State var endColorDayTime = Color(#colorLiteral(red: 1, green: 0.7960784314, blue: 0.2705882353, alpha: 1))
     
     var body: some View {
         ZStack {
             // 사용자의 시간이 18시 ~ 05시 사이
-            if hour < 5 || hour > 18 {
+//            if hour < 5 || hour > 18 {
                 LinearGradient(gradient: Gradient(colors: [startColorNightTime, endColorNightTime]), startPoint: .top, endPoint: .bottom)
                     .edgesIgnoringSafeArea(.all)
-            }
+//            }
             // 사용자의 시간이 05시 ~ 18시 사이
-            else {
-                LinearGradient(gradient: Gradient(colors: [startColorDayTime, endColorDayTime]), startPoint: .top, endPoint: .bottom)
-                    .edgesIgnoringSafeArea(.all)
-            }
+//            else {
+//                LinearGradient(gradient: Gradient(colors: [startColorDayTime, endColorDayTime]), startPoint: .top, endPoint: .bottom)
+//                    .edgesIgnoringSafeArea(.all)
+//            }
         }
         // GradientColor가 부드럽게 바뀌는 Animation
 //        .animation(.spring(response: 3,  blendDuration: 10))
