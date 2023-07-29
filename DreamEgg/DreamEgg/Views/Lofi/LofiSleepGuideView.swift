95c43344bf29fd969cbcbc75bcead62a4f8ab807
SeBin-Kwon <sbkwon98@gmail.com>
2023-07-29 20:29:44 +0900

    [Feat/#105] 1. 버튼 삭제 및 자동으로 라벨이 넘어가는 애니메이션 추가 2. 라벨 넘어가는 시간 변수 추가

--- DreamEgg/DreamEgg/Views/Lofi/LofiSleepGuideView.swift
+++ DreamEgg/DreamEgg/Views/Lofi/LofiSleepGuideView.swift
@@ -22,6 +22,7 @@ struct LofiSleepGuideView: View {
     
     @State private var sleepGuideSteps: SleepGuideSteps = .start
     @State private var afterHold: Bool = false
+    @State private var labelDelayTime: Double = 2.0
     @StateObject private var eggAnimation = EggAnimation()
     @Binding var isSkippedFromInteractionView: Bool
     
@@ -99,7 +100,7 @@ struct LofiSleepGuideView: View {
                     
                     Spacer()
                     
-                    if sleepGuideSteps != .darkening {
+                    if sleepGuideSteps == .hug {
                         Button {
                             sleepStepSwitcher()
                         } label: {
@@ -118,9 +119,12 @@ struct LofiSleepGuideView: View {
                         .padding(.vertical, 32)
                         .padding(.horizontal)
                     } else if sleepGuideSteps == .darkening {
-                        // - !!!: 상단의 알 Position이 차지할 수 있는 available Space를 제한하기 위한 Spacer
                         Spacer()
-                            .frame(maxHeight: 75)
+                            .frame(maxHeight: 80)
+                    }
+                    else {
+                        Spacer()
+                            .frame(maxHeight: 115)
                     }
                 }
                 
@@ -128,6 +132,8 @@ struct LofiSleepGuideView: View {
                     Button {
                         withAnimation {
                             sleepGuideSteps = .start
+                            labelDelayTime = 2.0
+                            repeatLabelChanging()
                         }
                     } label: {
                         Text("다시 하기")
@@ -147,6 +153,7 @@ struct LofiSleepGuideView: View {
                 if isSkippedFromInteractionView {
                     sleepGuideSteps = .darkening
                 }
+                repeatLabelChanging()
             }
             .onChange(of: scenePhase) { newValue in
                 if isChangingFromInactiveScene(into: newValue) {
@@ -175,7 +182,7 @@ struct LofiSleepGuideView: View {
             return Text("호흡에 집중하면서\n오늘 잠들 준비가 되셨나요?")
 //            return Text("Feel your body relaxing and focus.")
         case .darkening:
-            return Text("홀드 버튼을 눌러서\n알이 잘 수 있도록 불을 꺼주세요.")
+            return Text("잠금 버튼을 눌러서\n알이 잘 수 있도록 불을 꺼주세요.")
 //            return Text("Push the Lock button\nTo incubate the egg.")
         }
     }
@@ -204,14 +211,17 @@ struct LofiSleepGuideView: View {
         case .start:
             withAnimation {
                 sleepGuideSteps = .breath
+                labelDelayTime = 4.0
             }
         case .breath:
             withAnimation {
                 sleepGuideSteps = .release
+                labelDelayTime = 5.0
             }
         case .release:
             withAnimation {
                 sleepGuideSteps = .hug
+                labelDelayTime = 5.0
             }
         case .hug:
             withAnimation {
@@ -220,6 +230,20 @@ struct LofiSleepGuideView: View {
             
         case .darkening:
             print()
+        }
+        repeatLabelChanging()
+    }
+    
+    private func repeatLabelChanging() {
+        
+        switch sleepGuideSteps
+        {
+        case .hug:
+            return
+        default:
+            DispatchQueue.main.asyncAfter(deadline: .now() + labelDelayTime) {
+                sleepStepSwitcher()
+            }
         }
     }
     
