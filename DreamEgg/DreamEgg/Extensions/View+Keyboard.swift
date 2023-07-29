0ff7d68e9fef612511619d75219dbc35e02113b3
valselee <sollleky72@gmail.com>
2023-07-29 23:46:29 +0900

    [Add/#104] 1. View의 밖을 탭하면 키보드를 내리는 코드 이동

--- DreamEgg/DreamEgg/Extensions/View+Keyboard.swift
+++ DreamEgg/DreamEgg/Extensions/View+Keyboard.swift
@@ -5,4 +5,12 @@
 //  Created by Celan on 2023/07/29.
 //
 
-import Foundation
+import SwiftUI
+
+/// 빈 View를 탭하면 키보드가 내려갑니다.
+extension View {
+    func endTextEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
